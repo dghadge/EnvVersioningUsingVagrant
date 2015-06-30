3cf8369c3d2bee40543c365b4ad75d4569736406
dghadge <nyy021@gmail.com>
2015-06-29 23:28:02 -0400

    changed provisioning script to remove default folders after nginx install and copy checked in files to those locations

--- provision.sh
+++ provision.sh
@@ -2,8 +2,11 @@ apt-get -y update
 
 apt-get -y install nginx
 
-rm -rf /usr/share/nginx/www
+#provisioning should remove the default folders from nginx install process & copy checked-in files to those locations.
+#remove the sites-enabled folder created by nginx install process
+rm -rf /etc/nginx/sites-enabled
+#copy checked-in configuration files to nginx location
+cp -r /vagrant/sites-enabled /etc/nginx
 
-ln -s /vagrant/www/ /usr/share/nginx/www
 
 service nginx start
